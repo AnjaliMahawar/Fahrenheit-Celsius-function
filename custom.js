8f87a21b48ece18df1e9d925684eefaca189200f
Anjali MahawR <anjalimahawar454@gmail.com>
2022-06-20 11:03:55 +0530

    C TO F

---
+++ custom.js
@@ -0,0 +1,9 @@
+//function Defination
+function fahrenheitToCelsius(f){  //formal argument
+
+    var c =(f-32)*5/9 ;
+    console.log(`${f}   degrees fahrenheit is equal to ${c} degrees celsius .`)
+}
+//function Calling   //many time process
+fahrenheitToCelsius(50)  //actual arguments
+fahrenheitToCelsius(60) 
